6ffafe87be592c8eba652a1a1229bd6d8d9e5d82
Aaron Ciuffo <aaron.ciuffo@gmail.com>
2021-09-10 09:24:07 +0200

    add a & b button for setting time

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-    	
+    change_by = 1
+    if (set_time_mode == true) {
+        set_time_array = [kitronik_halo_hd.readTimeParameter(TimeParameter.Seconds), kitronik_halo_hd.readTimeParameter(TimeParameter.Minutes), kitronik_halo_hd.readTimeParameter(TimeParameter.Hours)]
+        set_time_array[1] = set_time_array[1] + change_by
+        if (set_time_array[1] > 59) {
+            set_time_array[1] = 0
+            set_time_array[2] = set_time_array[2] + 1
+        }
+        if (set_time_array[2] > 12) {
+            set_time_array[2] = 0
+        }
+        kitronik_halo_hd.setTime(set_time_array[2], set_time_array[1], set_time_array[0])
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -13,6 +23,21 @@ input.onButtonPressed(Button.AB, function () {
         . . . . .
         . . . . .
         `)
+})
+input.onButtonPressed(Button.B, function () {
+    change_by = 10
+    if (set_time_mode == true) {
+        set_time_array = [kitronik_halo_hd.readTimeParameter(TimeParameter.Seconds), kitronik_halo_hd.readTimeParameter(TimeParameter.Minutes), kitronik_halo_hd.readTimeParameter(TimeParameter.Hours)]
+        set_time_array[1] = set_time_array[1] + change_by
+        if (set_time_array[1] > 59) {
+            set_time_array[1] = 0
+            set_time_array[2] = set_time_array[2] + 1
+        }
+        if (set_time_array[2] > 12) {
+            set_time_array[2] = 0
+        }
+        kitronik_halo_hd.setTime(set_time_array[2], set_time_array[1], set_time_array[0])
+    }
 })
 function display_smh (time_array: any[], clear: boolean) {
     if (clear) {
@@ -48,6 +73,8 @@ function set_time_indicator () {
 }
 let old_time_array: number[] = []
 let set_color: number[] = []
+let set_time_array: number[] = []
+let change_by = 0
 let set_time_mode = false
 let time_array: number[] = []
 let haloDisplay: kitronik_halo_hd.ZIPHaloHd = null
@@ -59,11 +86,15 @@ haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
 time_array = [kitronik_halo_hd.readTimeForZip(TimeParameter.Seconds), kitronik_halo_hd.readTimeForZip(TimeParameter.Minutes), kitronik_halo_hd.readTimeForZip(TimeParameter.Hours)]
 let last_second = time_array[0]
 set_time_mode = false
+basic.showString("..")
 basic.forever(function () {
     old_time_array = time_array
     time_array = [kitronik_halo_hd.readTimeForZip(TimeParameter.Seconds), kitronik_halo_hd.readTimeForZip(TimeParameter.Minutes), kitronik_halo_hd.readTimeForZip(TimeParameter.Hours)]
     if (set_time_mode == true) {
         set_time_indicator()
+        display_smh(old_time_array, true)
+        display_smh(time_array, false)
+        haloDisplay.show()
     } else {
         if (last_second != time_array[0]) {
             display_smh(old_time_array, true)
